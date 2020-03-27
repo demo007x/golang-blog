6f87d2c1f3307c65a22c385af5c4a37157d74859
杨玉龙 <anziguoer@163.com>
2020-03-27 22:44:00 +0800

    update

--- utils/common.go
+++ utils/common.go
@@ -125,3 +125,35 @@ func AppUrl(path string) string {
 	}
 	return fmt.Sprintf("%s%s", appUrl, path)
 }
+
+// SocialHtml 返回社交html
+func SocialHtml() string  {
+	var socialHtml string
+
+	config := config.Config
+	socialSection,err := config.GetSection("social")
+	if err != nil {
+		return socialHtml
+	}
+
+	// 定义映射关系
+	socialMap := make(map[string]string)
+	socialMap["github"] = `<i class="fab fa-github" aria-hidden="true"></i>`
+	socialMap["twitter"] = `<i class="fab fa-twitter" aria-hidden="true"></i>`
+	socialMap["linkedin"] = `<i class="fab fa-linkedin" aria-hidden="true"></i>`
+	socialMap["stack"] = `<i class="fab fa-stack-overflow" aria-hidden="true"></i>`
+	socialMap["codepen"] = `<i class="fab fa-codepen" aria-hidden="true"></i>`
+
+	// <li class="list-inline-item"><a href="#"><i class="fab fa-twitter fa-fw"></i></a></li>
+
+	for _, key := range socialSection.KeyStrings() {
+		val, _ := socialSection.GetKey(key)
+		if keyHtml,ok := socialMap[key]; ok {
+			socialHtml = fmt.Sprintf(`%s<li class="list-inline-item"><a href="%s">%s</a></li>`, socialHtml, val, keyHtml)
+		} else {
+			socialHtml = fmt.Sprintf(`%s<li class="list-inline-item"><a href="%s">%s</a></li>`, socialHtml, val, key)
+		}
+	}
+
+	return socialHtml
+}
